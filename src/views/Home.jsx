7c3ed688bdb894f82e4bc9106cee660d84b4a78b
Tacha <degiovanni21tach@gmail.com>
2021-01-14 16:54:55 +0100

    conflicts

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,19 +1,5 @@
-<<<<<<< HEAD
 import React from "react";
-import Navbar from "../components/Layout/Navbar";
-import HomeFrame from "../components/Layout/HomeFrame";
-export default function Home() {
-  return (
-    <div className="Home">
-      <div>
-        <Navbar />
-        <HomeFrame />
-      </div>
-    </div>
-  );
-=======
-import React from 'react';
-import { dataBase } from '../components/Api';
+import { dataBase } from "../components/Api";
 
 class Home extends React.Component {
   render() {
@@ -22,13 +8,13 @@ class Home extends React.Component {
     return (
       <div>
         <h1>Binouze</h1>
-        <h2>Achètes en mets en fav tes bières</h2>
-        <div className='beercard'>
+        <h2>Achètes et mets en fav tes bières</h2>
+        <div className="beercard">
           {bieres.map((biere) => (
             <>
               <p>{biere.id}</p>
               <h3>{biere.name}</h3>
-              <img src={biere.picture} alt='bière' />
+              <img src={biere.picture} alt="bière" />
               <p>{biere.text}</p>
               <p>{biere.price}</p>
             </>
@@ -37,7 +23,6 @@ class Home extends React.Component {
       </div>
     );
   }
->>>>>>> b03cc36f64904eeb1cdc17822dc4fc85190ad6d0
 }
 
 export default Home;
