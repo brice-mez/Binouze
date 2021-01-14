d6c03b7a01c1daae23cdc7f6961f026a3f5fdb7f
bryce-mez <brice.mezerette.pro@gmail.com>
2021-01-14 11:02:00 +0100

    contact css

--- src/views/Contact.jsx
+++ src/views/Contact.jsx
@@ -1,13 +1,21 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import LocalBarIcon from '@material-ui/icons/LocalBar';
+import './Contact.css';
 
 const useStyles = makeStyles((theme) => ({
   root: {
     '& > *': {
-      margin: theme.spacing(1),
+      margin: theme.spacing(4.5),
       width: '25ch',
+      borderColor: '#947349',
     },
+  },
+  button: {
+    margin: theme.spacing(1),
+    backgroundColor: '#947349',
   },
 }));
 
@@ -15,21 +23,37 @@ export default function Contact() {
   const classes = useStyles();
 
   return (
-    <form className={classes.root} noValidate autoComplete='off'>
-      <TextField id='outlined-basic-name' label='Outlined' variant='outlined' />
-      <TextField
-        id='outlined-basic-email'
-        label='Outlined'
-        variant='outlined'
-      />
-      <TextField
-        id='outlined-multiline-static'
-        label='Multiline'
-        multiline
-        rows={4}
-        defaultValue='Default Value'
-        variant='outlined'
-      />
-    </form>
+    <div className='main'>
+      <form className={classes.root} noValidate autoComplete='off'>
+        <TextField
+          id='outlined-basic-name'
+          label='Pseudo'
+          variant='outlined'
+          color='default'
+        />
+        <TextField
+          id='outlined-basic-email'
+          label='Email'
+          variant='outlined'
+          color='default'
+        />
+        <TextField
+          id='outlined-multiline-static'
+          label='Commentaire'
+          multiline
+          rows={4}
+          variant='outlined'
+          color='default'
+        />
+        <Button
+          variant='contained'
+          color='default'
+          className={classes.button}
+          endIcon={<LocalBarIcon>Drink</LocalBarIcon>}
+        >
+          DRINK
+        </Button>
+      </form>
+    </div>
   );
 }
