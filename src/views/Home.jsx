227c6e7f294772fdc6a75b12a83e184624ada113
Tacha <degiovanni21tach@gmail.com>
2021-01-14 09:18:26 +0100

    icons

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,5 +1,10 @@
 import React from "react";
-
+import ShoppingCart from "../components/icons/AddShoppingCart";
 export default function Home() {
-  return <h1>Binouze</h1>;
+  return (
+    <div>
+      <h1>Binouze</h1>
+      <ShoppingCart />
+    </div>
+  );
 }
