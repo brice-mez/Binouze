26a6146f1d4b59a7c1c400ce9093bef83b870995
Tacha <degiovanni21tach@gmail.com>
2021-01-14 15:32:38 +0100

    Home CSS

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import Navbar from "../components/Layout/Navbar";
 import HomeFrame from "../components/Layout/HomeFrame";
+import "./Home.css";
 export default function Home() {
   return (
-    <div>
-      <Navbar />
-      <HomeFrame />
+    <div className="Home">
+      <div>
+        <Navbar />
+        <HomeFrame />
+      </div>
     </div>
   );
 }
