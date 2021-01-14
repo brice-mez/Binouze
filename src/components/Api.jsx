78576a11313a446d9e632d41d716e0a8e9404f82
bryce-mez <brice.mezerette.pro@gmail.com>
2021-01-14 16:16:18 +0100

    map api

--- src/components/Api.jsx
+++ src/components/Api.jsx
@@ -1,20 +1,10 @@
-import chouffe from '../images/chouffe.jpg';
-import corona from '../images/corona.jpg';
-import cuvéetroll from '../images/cuvéetroll.png';
-import delitremens from '../images/delitremens.jpg';
-import demon from '../images/demon.jpg';
-import duvel from '../images/duvel.jpg';
-import jupiler from '../images/jupiler.jpg';
-import kwak from '../images/kwak.jpg';
-import molson from '../images/molson.jpg';
-import paixdieu from '../images/paixdieu.jpg';
-
 export const dataBase = [
   {
     id: 1,
     name: 'Paix Dieu',
     text: "L'unique bière d'abbaye brassée lors d'une pleine lune.",
-    picture: { paixdieu },
+    picture:
+      'https://d33za54wpumlhy.cloudfront.net/eyJidWNrZXQiOiJzYXZldXJiaWVyZSIsImtleSI6ImltZy9wLzUxNTMtMTU2MDQuanBnIiwiZWRpdHMiOnsid2VicCI6eyJxdWFsaXR5Ijo4MH0sInBuZyI6eyJxdWFsaXR5Ijo4MH0sInJlc2l6ZSI6eyJ3aWR0aCI6MzI4LCJoZWlnaHQiOjIyNSwiZml0IjoiY29udGFpbiIsImJhY2tncm91bmQiOnsiciI6MCwiZyI6MCwiYiI6MCwiYWxwaGEiOjB9fX19',
     price: '2.5€',
   },
   {
@@ -22,7 +12,8 @@ export const dataBase = [
     name: 'La bière du démon',
     text:
       'Une bière blonde au goût puissant et typé qui compte parmis les bières les plus fortes au monde !',
-    picture: { demon },
+    picture:
+      'https://www.carrefour.fr/media/540x540/Photosite/PGC/BOISSONS/3261570000136_PHOTOSITE_20200311_052830_0.jpg?placeholder=1',
     price: '2.5€',
   },
   {
@@ -30,7 +21,8 @@ export const dataBase = [
     name: 'Corona Extra',
     text:
       'La bière blonde la plus vendue au Mexique. Idéale pour accompagner les plats épicés !',
-    picture: { corona },
+    picture:
+      'https://produits.bienmanger.com/34491-0w600h600_Corona_Extra_Biere_Blonde_Mexicaine.jpg',
     price: '2.5€',
   },
   {
@@ -38,7 +30,8 @@ export const dataBase = [
     name: 'Jupiler',
     text:
       'La Jupiler est une bière blonde de fermentation basse et de type Pils brassée depuis 1966 en Belgique.',
-    picture: { jupiler },
+    picture:
+      'https://boutique.maltsethoublons.com/media/catalog/product/cache/16/image/9df78eab33525d08d6e5fb8d27136e95/2/3/2328-biere-jupiler-bouteille-33cl.jpg',
     price: '2.5€',
   },
   {
@@ -46,7 +39,8 @@ export const dataBase = [
     name: 'Delirium Tremens',
     text:
       "Médaille d'or lors du World Beer Championship, une excellente bière blonde aux 3 levures !",
-    picture: { delitremens },
+    picture:
+      'https://produits.bienmanger.com/29293-0w600h600_Delirium_Tremens_Biere_Blonde_Belge.jpg',
     price: '2.5€',
   },
   {
@@ -54,7 +48,8 @@ export const dataBase = [
     name: 'Molson Canadian',
     text:
       "Une Lager brassée avec de l'eau canadienne et de l'orge des prairies.",
-    picture: { molson },
+    picture:
+      'https://produits.bienmanger.com/27695-0w470h470_Biere_Canadienne_Molson.jpg',
     price: '2.5€',
   },
   {
@@ -62,7 +57,8 @@ export const dataBase = [
     name: 'Kwak',
     text:
       "Une bière belge qu'on ne présente plus, à accompagner de son verre du cocher !",
-    picture: { kwak },
+    picture:
+      'https://www.carrefour.fr/media/1500x1500/Photosite/PGC/BOISSONS/0000054050051_PHOTOSITE_20200616_092759_0.jpg?placeholder=1',
     price: '2.5€',
   },
   {
@@ -70,14 +66,16 @@ export const dataBase = [
     name: 'Cuvée des trolls',
     text:
       "Une bière artisanale aux notes fruitées d'orange et de miel. Une référence en bière belge !",
-    picture: { cuvéetroll },
+    picture:
+      'https://www.carrefour.fr/media/1500x1500/Photosite/PGC/BOISSONS/5411551141091_PHOTOSITE_20201105_190518_0.jpg?placeholder=1',
     price: '2.5€',
   },
   {
     id: 9,
     name: 'La Chouffe',
     text: 'La bière belge du lutin au bonnet rouge !',
-    picture: { chouffe },
+    picture:
+      'https://media-verticommnetwork1.netdna-ssl.com/wines/la-chouffe-440878-s77_e.jpg',
     price: '2.5€',
   },
   {
@@ -85,7 +83,8 @@ export const dataBase = [
     name: 'Duvel',
     text:
       'Une bière belge forte à la célébrité internationale qui présente des qualités exceptionnelles !',
-    picture: { duvel },
+    picture:
+      'https://produits.bienmanger.com/13993-0w600h600_Biere_Duvel_Blonde.jpg',
     price: '2.5€',
   },
 ];
