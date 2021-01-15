5ea594da8c31ba7d219b23633f0f89a519c75e21
Tacha <degiovanni21tach@gmail.com>
2021-01-15 09:33:25 +0100

    Cart Update

--- src/components/icons/ShoppingCart.jsx
+++ src/components/icons/ShoppingCart.jsx
@@ -1,9 +1,12 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Badge, IconButton } from "@material-ui/core";
 import { ShoppingCart } from "@material-ui/icons";
 import { Link } from "react-router-dom";
+import { ShoppingCartContext } from "../../App";
 
 const Cart = () => {
+  const { itemsInCart } = useContext(ShoppingCartContext);
+  const n = itemsInCart ? itemsInCart.length : 0;
   return (
     <IconButton
       component={Link}
@@ -11,7 +14,7 @@ const Cart = () => {
       aria-label="Show cart items"
       color="inherit"
     >
-      <Badge badgeContent={2} color="secondary">
+      <Badge badgeContent={n} color="secondary">
         <ShoppingCart />
       </Badge>
     </IconButton>
