8e67b5577f47b782b5d31926e6b6b1d9a3e8dd17
bryce-mez <brice.mezerette.pro@gmail.com>
2021-01-15 10:48:30 +0100

    react helmet snap

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,10 +1,17 @@
-import React from "react";
-import Navbar from "../components/Layout/Navbar";
-import Beers from "../components/Layout/Beers";
+import React from 'react';
+import Navbar from '../components/Layout/Navbar';
+import Beers from '../components/Layout/Beers';
+import { Helmet } from 'react-helmet';
 
 function Home() {
   return (
     <div>
+      <Helmet>
+        <meta charSet='utf-8' />
+        <title>Binouze</title>
+        <link rel='canonical' href='http://mysite.com/exemple' />
+        <meta name='description' content='Acheter des bières' />
+      </Helmet>
       <Navbar />
       <Beers />
     </div>
